ab20a89cfce0fbf56523e265d72e168d8b900a45
Jakub Toda <todziaktoda@gmail.com>
2024-01-08 09:36:55 +0100

    zalącznik nr 2 w main.cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+echo "3include <stdlib>" > main.cpp
+echo "using namespace std;" >> main.cpp
+echo "int main(){" >> main.cpp
+echo "	cout << \"HelloWorld!\" << endl;" >> main.cpp
+echo " 	return 0;" >> main.cpp
+echo "}" >> main.cpp
